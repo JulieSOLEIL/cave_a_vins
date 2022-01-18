f243e972801dbce8107977d37f6486b78747e331
Julie SUN <sun.julie0@gmail.com>
2022-01-18 16:26:55 +0100

    ajout des fichiers

--- src/Controller/VinController.php
+++ src/Controller/VinController.php
@@ -2,20 +2,69 @@
 
 namespace App\Controller;
 
+use App\Entity\Vin;
+use App\Form\VinType;
+use Doctrine\ORM\Mapping\Id;
 use App\Repository\VinRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VinController extends AbstractController
 {
-    #[Route('/vin/list', name: 'vin.list')]
+    #[Route('/vin/list', name: 'vin.list', methods: ['GET'])]
     public function list(VinRepository $vinRepository): Response
     {
-        $vins = $vinRepository->findAll() ;
-        
         return $this->render('vin/list.html.twig', [
-            'vins' => $vins,
+            'vins' => $vinRepository->findAll(),
         ]);
     }
-}
+
+    #[Route('/vin/show/{id}', name: 'vin.show')]
+    public function show(Vin $vin): Response
+    {
+        return $this->render(
+            'vin/show.html.twig',
+            [
+                'vin' => $vin
+            ]
+        );
+    }
+
+    #[Route('/vin/new', name: 'vin.new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $vin = new Vin();
+        $form = $this->createForm(VinType::class, $vin);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($vin);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('vin.list', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('vin/new.html.twig', [
+            'vin' => $vin,
+            'form' => $form, 
+        ]);
+    }
+
+    // #[Route('/vin/show/{id}', name: 'vin.show')]
+    // public function show(VinRepository $repo, Request $req): Response
+    // {
+    //     $id = $req->get(Id);
+
+    //     return $this->render(
+    //         'vin/show.html.twig',
+    //         [
+    //             'vin' => $vin
+    //         ]
+    //     );
+    // }
+
+}   
+
---
+++ src/Form/VinType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Vin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VinType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('vin')
+           
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Vin::class,
+        ]);
+    }
+}
