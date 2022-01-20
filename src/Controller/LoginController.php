a3b1c6ef758fc702fbf8311f482203c241e3bab6
SUN Julie <sun.julie0@gmail.com>
2022-01-20 10:01:23 +0100

    ajout fichiers

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function formLogin(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastEmail = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/formLogin.html.twig', [
+            'error' => $error,
+            'lastEmail' => $lastEmail
+        ]);
+    }
+   
+    #[Route('/logout', name:'userLogout')]
+    public function logout(): void
+   {
+       // controller can be blank: it will never be called!
+       throw new \Exception('Don\'t forget to activate logout in security.yaml');
+   }
+}
--- src/Controller/VinController.php
+++ src/Controller/VinController.php
@@ -7,11 +7,13 @@ use App\Form\VinType;
 use Doctrine\ORM\Mapping\Id;
 use App\Repository\VinRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[IsGranted('ROLE_ADMIN')]
 class VinController extends AbstractController
 {
     #[Route('/vin/list', name: 'vin.list', methods: ['GET'])]
